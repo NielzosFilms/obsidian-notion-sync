60dfebd653bd2da32219d2fdb928088f2da5bd97
NielzosFilms <nielzosfilms@gmail.com>
2022-07-08 22:21:49 +0200

    Initial commit

--- main.ts
+++ main.ts
@@ -1,14 +1,31 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+	App,
+	Editor,
+	MarkdownView,
+	Modal,
+	Notice,
+	Plugin,
+	PluginSettingTab,
+	Setting,
+} from "obsidian";
+
+import { DownloadFromNotion } from "./src/download-pages";
 
 // Remember to rename these classes and interfaces!
 
 interface MyPluginSettings {
-	mySetting: string;
+	notionAPI: string;
+	databaseID: string;
+	outputDir: string;
+	notionWorkspace: string;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+	notionAPI: "",
+	databaseID: "",
+	outputDir: "Notion",
+	notionWorkspace: "YOUR_NOTION_WORKSPACE_NAME",
+};
 
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
@@ -17,41 +34,48 @@ export default class MyPlugin extends Plugin {
 		await this.loadSettings();
 
 		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
+		const ribbonIconEl = this.addRibbonIcon(
+			"dice",
+			"Obsidian Notion Download",
+			(evt: MouseEvent) => {
+				// Called when the user clicks the icon.
+				const downloader = new DownloadFromNotion(this);
+				downloader.downloadPages();
+				new Notice("Check the console!");
+			}
+		);
 		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		ribbonIconEl.addClass("my-plugin-ribbon-class");
 
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
 		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		statusBarItemEl.setText("Status Bar Text");
 
 		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
+			id: "open-sample-modal-simple",
+			name: "Open sample modal (simple)",
 			callback: () => {
 				new SampleModal(this.app).open();
-			}
+			},
 		});
 		// This adds an editor command that can perform some operation on the current editor instance
 		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
+			id: "sample-editor-command",
+			name: "Sample editor command",
 			editorCallback: (editor: Editor, view: MarkdownView) => {
 				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
+				editor.replaceSelection("Sample Editor Command");
+			},
 		});
 		// This adds a complex command that can check whether the current state of the app allows execution of the command
 		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
+			id: "open-sample-modal-complex",
+			name: "Open sample modal (complex)",
 			checkCallback: (checking: boolean) => {
 				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
+				const markdownView =
+					this.app.workspace.getActiveViewOfType(MarkdownView);
 				if (markdownView) {
 					// If checking is true, we're simply "checking" if the command can be run.
 					// If checking is false, then we want to actually perform the operation.
@@ -62,7 +86,7 @@ export default class MyPlugin extends Plugin {
 					// This command will only show up in Command Palette when the check function returns true
 					return true;
 				}
-			}
+			},
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
@@ -70,20 +94,24 @@ export default class MyPlugin extends Plugin {
 
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
 		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
+		this.registerDomEvent(document, "click", (evt: MouseEvent) => {
+			console.log("click", evt);
 		});
 
 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.registerInterval(
+			window.setInterval(() => console.log("setInterval"), 5 * 60 * 1000)
+		);
 	}
 
-	onunload() {
-
-	}
+	onunload() {}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
 	}
 
 	async saveSettings() {
@@ -97,12 +125,12 @@ class SampleModal extends Modal {
 	}
 
 	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+		const { contentEl } = this;
+		contentEl.setText("Woah!");
 	}
 
 	onClose() {
-		const {contentEl} = this;
+		const { contentEl } = this;
 		contentEl.empty();
 	}
 }
@@ -116,22 +144,68 @@ class SampleSettingTab extends PluginSettingTab {
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl("h2", {
+			text: "Settings for Obsidian Notion download.",
+		});
+
+		const notionApiInput = new Setting(containerEl)
+			.setName("Notion API Token")
+			.setDesc("It's a secret")
+			.addText((text) =>
+				text
+					.setPlaceholder("Enter your Notion API Token")
+					.setValue(this.plugin.settings.notionAPI)
+					.onChange(async (value) => {
+						this.plugin.settings.notionAPI = value;
+						await this.plugin.saveSettings();
+					})
+			);
+		notionApiInput.controlEl.querySelector("input").type = "password";
+
+		const notionDatabaseId = new Setting(containerEl)
+			.setName("Notion Database ID")
+			.setDesc("It's a secret")
+			.addText((text) =>
+				text
+					.setPlaceholder("Enter your Notion database ID")
+					.setValue(this.plugin.settings.databaseID)
+					.onChange(async (value) => {
+						this.plugin.settings.databaseID = value;
+						await this.plugin.saveSettings();
+					})
+			);
+		notionDatabaseId.controlEl.querySelector("input").type = "password";
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName("Obsidian output directory")
+			.setDesc("Local output directory of the downloaded Notion pages")
+			.addText((text) =>
+				text
+					.setPlaceholder("Enter the output directory")
+					.setValue(this.plugin.settings.outputDir)
+					.onChange(async (value) => {
+						this.plugin.settings.outputDir = value;
+						await this.plugin.saveSettings();
+					})
+			);
+
+		new Setting(containerEl)
+			.setName("Notion workspace name")
+			.setDesc(
+				"Your Notion workspace name to create a url on the local files"
+			)
+			.addText((text) =>
+				text
+					.setPlaceholder("Your Notion workspace")
+					.setValue(this.plugin.settings.notionWorkspace)
+					.onChange(async (value) => {
+						this.plugin.settings.notionWorkspace = value;
+						await this.plugin.saveSettings();
+					})
+			);
 	}
 }
---
+++ src/download-pages.ts
@@ -0,0 +1,118 @@
+import { requestUrl } from "obsidian";
+import type { Blocks } from "@notion-stuff/v4-types";
+import MyPlugin from "../main";
+
+import { NotionToMarkdown } from "notion-to-md";
+
+import { Client } from "@notionhq/client";
+
+interface SimpleMarkdownPage {
+	title: string;
+	id: string;
+	content: string;
+}
+
+export class DownloadFromNotion {
+	app: MyPlugin;
+	agent: any;
+
+	constructor(app: MyPlugin) {
+		this.app = app;
+	}
+
+	public async downloadPages() {
+		const pages = await this.getPages();
+		const markdownPages: SimpleMarkdownPage[] = [];
+
+		const parser = new NotionToMarkdown({ notionClient: new Client() });
+
+		await Promise.all(
+			// Use map instead of foreach to do this in parallel
+			pages.map(async (page) => {
+				const blocks = await this.getPageBlocks(page.id);
+
+				markdownPages.push({
+					title: page.properties.Name.title[0].plain_text,
+					id: page.id,
+					content: parser.toMarkdownString(
+						await parser.blocksToMarkdown(blocks)
+					),
+				});
+			})
+		);
+		this.writeToFiles(markdownPages);
+	}
+
+	private async writeToFiles(mdPages: SimpleMarkdownPage[]) {
+		const vault = this.app.app.vault;
+		const files = vault.getFiles();
+		mdPages.forEach((page) => {
+			const file = files.find(
+				(file) => file.path === this.getPageFilePath(page)
+			);
+			if (file) {
+				vault.modify(file, this.getFileHeader(page) + page.content);
+			} else {
+				vault.create(
+					this.getPageFilePath(page),
+					this.getFileHeader(page) + page.content
+				);
+			}
+		});
+	}
+
+	private getPageFilePath(mdPage: SimpleMarkdownPage): string {
+		return `${this.app.settings.outputDir}/${mdPage.title}_${mdPage.id}.md`;
+	}
+
+	private getFileHeader(mdPage: SimpleMarkdownPage): string {
+		return `%%
+Note: This page was downloaded from Notion, any changes will be overwritten.
+%%
+links: [[Notion]]
+notion-url: [${mdPage.title}](https://www.notion.so/${
+			this.app.settings.notionWorkspace
+		}/${mdPage.title.split(" ").join("-")}-${mdPage.id})
+
+---
+`;
+	}
+
+	private async getPages(): Promise<any[]> {
+		const bodyString: any = {
+			page_size: 100,
+		};
+		const response = await requestUrl({
+			url: `https://api.notion.com/v1/databases/${this.app.settings.databaseID}/query`,
+			method: "POST",
+			headers: {
+				Accept: "application/json",
+				// 'User-Agent': 'obsidian.md',
+				Authorization: "Bearer " + this.app.settings.notionAPI,
+				"Notion-Version": "2021-08-16",
+			},
+			body: JSON.stringify(bodyString),
+		});
+		return response.json.results;
+	}
+
+	private async getPageBlocks(pageId: string): Promise<Blocks> {
+		const response = await requestUrl({
+			url: `https://api.notion.com/v1/blocks/${pageId}/children?page_size=100`,
+			method: "GET",
+			headers: {
+				Accept: "application/json",
+				// 'User-Agent': 'obsidian.md',
+				Authorization: "Bearer " + this.app.settings.notionAPI,
+				"Notion-Version": "2021-08-16",
+			},
+		});
+		return response.json.results.map((block: any) => {
+			// after a lot of searching in the types I fount out that this is not mapped correctly :angry:
+			if (block[block.type]?.text) {
+				block[block.type].rich_text = block[block.type].text;
+			}
+			return block;
+		}) as Blocks;
+	}
+}
